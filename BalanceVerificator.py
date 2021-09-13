b048c9ada4ea7dcb250decb063f2aa75c0cb9e3e
snaiper20a2 <yaroslav.imshenetskyi@nure.ua>
2021-09-13 16:41:41 +0300

    code-style fixes

--- BalanceVerificator.py
+++ BalanceVerificator.py
@@ -1,29 +1,40 @@
+"""
+Brackets balancing module
+"""
 from collections import deque
 import re
 
 
 class BalanceVerificator:
-    OPEN_BRACKET = ['(', '{', '[']
-    CLOSE_BRACKET = [')', '}', ']']
-    PATTERN = r'[^\(\)\[\]\{\}]'
+  """
+  This class is for checking brackets balancing.
+  Brackets are considered matched pair if it occurs that every open bracket has
+  closing bracket of the exact the same type (‘(’, ‘{’, ‘[’)
+  """
+  OPEN_BRACKET = ['(', '{', '[']
+  CLOSE_BRACKET = [')', '}', ']']
+  PATTERN = r'[^\(\)\[\]\{\}]'
 
-    def verify(self, data: str):
-        if data == '':
-            raise ValueError('Input line is empty')
-        match = re.search(self.PATTERN, data)
-        if match:
-            raise ValueError('A character ‘{}’ doesn’t belong to any known brackets type'.format(match[0]))
-        bracket_stack = deque()
-        for letter in data:
-            if self.OPEN_BRACKET.count(letter) == 1:
-                bracket_stack.append(letter)
-                continue
-            elif self.CLOSE_BRACKET.count(letter) == 1:
-                open_bracket = self.OPEN_BRACKET[self.CLOSE_BRACKET.index(letter)]
-                if bracket_stack.pop() == open_bracket:
-                    continue
-                else:
-                    print('NOT BALANCED')
-                    return len(bracket_stack) + 1
-        print('BALANCED')
-        return -1
+  def verify(self, data: str):
+    """
+    :param data: string with brackets
+    :return: -1 if string is balanced or number of not balanced bracket
+    """
+    if data == '':
+      raise ValueError('Input line is empty')
+    match = re.search(self.PATTERN, data)
+    if match:
+      raise ValueError('A character ‘{}’ doesn’t belong '
+                       'to any known brackets type'.format(match[0]))
+    bracket_stack = deque()
+    for letter in data:
+      if self.OPEN_BRACKET.count(letter) == 1:
+        bracket_stack.append(letter)
+        continue
+      if self.CLOSE_BRACKET.count(letter) == 1:
+        open_bracket = self.OPEN_BRACKET[self.CLOSE_BRACKET.index(letter)]
+        if bracket_stack.pop() != open_bracket:
+          print('NOT BALANCED')
+          return len(bracket_stack) + 1
+    print('BALANCED')
+    return -1
--- tests.py
+++ tests.py
@@ -4,31 +4,30 @@ from BalanceVerificator import BalanceVerificator
 
 class MyTestCase(unittest.TestCase):
 
-    def test_1(self):
-        balance_verificator = BalanceVerificator()
-        self.assertEqual(balance_verificator.verify('{[(())]}'), -1)
+  def test_1(self):
+    balance_verificator = BalanceVerificator()
+    self.assertEqual(balance_verificator.verify('{[(())]}'), -1)
 
-    def test_2(self):
-        balance_verificator = BalanceVerificator()
-        self.assertEqual(balance_verificator.verify('{[(]}'), 3)
+  def test_2(self):
+    balance_verificator = BalanceVerificator()
+    self.assertEqual(balance_verificator.verify('{[(]}'), 3)
 
-    def test_3(self):
-        balance_verificator = BalanceVerificator()
-        self.assertEqual(balance_verificator.verify('{{[[[[[(())]]]]]}}'), -1)
+  def test_3(self):
+    balance_verificator = BalanceVerificator()
+    self.assertEqual(balance_verificator.verify('{{[[[[[(())]]]]]}}'), -1)
 
-    def test_4(self):
-        balance_verificator = BalanceVerificator()
-        self.assertEqual(balance_verificator.verify('{{()]}}'), 2)
+  def test_4(self):
+    balance_verificator = BalanceVerificator()
+    self.assertEqual(balance_verificator.verify('{{()]}}'), 2)
 
-    def test_5(self):
-        balance_verificator = BalanceVerificator()
-        self.assertRaises(ValueError, balance_verificator.verify, '{[()]};')
+  def test_5(self):
+    balance_verificator = BalanceVerificator()
+    self.assertRaises(ValueError, balance_verificator.verify, '{[()]};')
 
-    def test_6(self):
-        balance_verificator = BalanceVerificator()
-        self.assertRaises(ValueError, balance_verificator.verify, '')
-
+  def test_6(self):
+    balance_verificator = BalanceVerificator()
+    self.assertRaises(ValueError, balance_verificator.verify, '')
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
